20ac53edd99a46e281cfe5e70b3fbe275be74702
jalen <jalenmmoorer@gmail.com>
2017-03-14 00:58:26 -0400

    Added Appbar and Layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Button from 'react-toolbox/lib/button/Button';
+
+import Layout from 'react-toolbox/lib/layout/Layout';
+import Panel from 'react-toolbox/lib/layout/Panel';
+import Sidebar from 'react-toolbox/lib/layout/Sidebar';
+
+import AppBar from 'react-toolbox/lib/app_bar/AppBar';
+import IconButton from 'react-toolbox/lib/button/IconButton';
+
 import './App.css';
 
 class App extends Component {
+   
+   state = {
+        sidebarPinned: true
+    };
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <div className="App-wrapper">
-        <Button raised primary>Test</Button>
-          <div className="App-preview"></div>
-          <div className="App-content"></div>
-        </div>
-      </div>
+        <Layout>
+            <Panel>
+            <AppBar Title='React App' leftIcon='menu' fixed flat/>
+                <div style={{ position: 'fixed', flex: 1, overflowY: 'auto', padding: '1.8rem', top: '6.4rem' }}>
+                    <h1>Main Content</h1>
+                    <p>Main content goes here.</p>
+                </div>
+            </Panel>
+            <Sidebar pinned={ this.state.sidebarPinned } width={12}>
+                <div style={{ flex: 1 }}>
+                    <p>Supplemental content goes here.</p>
+                </div>
+            </Sidebar>
+        </Layout>
     );
   }
 }
